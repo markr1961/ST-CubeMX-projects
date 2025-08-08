be95b12102d40da2665853b16e2c354b79b03064
Mark Rosenau <mark.rosenau@currentlighting.com>
2025-08-08 10:17:18 -0500

    initial commit for AdaFruit_LCD_SD_joystick and L073RZ_blinky projects

---
+++ AdaFruit_LCD_SD_joystick/readme.md
@@ -0,0 +1,19 @@
+
+IAR WARM v8.40
+
+
+### [Adafruit 1.8" Color TFT Shield w/microSD and Joystick - v 2 $34.95](https://www.adafruit.com/product/802)
+[blog:  Adafruit 1.8 18-bit Color TFT Shield w/microSD and Joystick](https://blog.adafruit.com/2012/04/26/new-product-adafruit-1-8-18-bit-color-tft-shield-wmicrosd-and-joystick/)  
+This lovely little shield is the best way to add a small, colorful and bright display to any project. We took our popular 1.8″ TFT breakout board and remixed it into an Arduino shield complete with microSD card slot and a 5-way joystick navigation switch (with a nice plastic knob)! Since the display uses only 4 pins to communicate and has its own pixel-addressable frame buffer, it can be used easily to add a display & interface without exhausting the memory or pins.
+##### Specifications:
+1.8″ diagonal LCD TFT display  
+128×160 resolution, 18-bit (262,144) color  
+ST7735R (datasheet) controller with built in pixel-addressable video RAM buffer  
+4 wire SPI digital interface  
+Built-in microSD slot – uses 2 more digital lines  
+5V compatible! Use with 3.3V or 5V logic Arduinos  
+Onboard 3.3V @ 150mA LDO regulator  
+2 white LED backlight, transistor connected. Defaults to ‘on’ but can be connected to a digital pin for PWM or simply turning off  
+Comes with header, requires soldering!  
+
+There is example projects in CubeMX for L073 and the binary demonstration files are available from the STM32L073RZ-Nucleo page in the Binary Resources section. see: https://www.st.com/en/evaluation-tools/nucleo-l073rz.html
---
+++ L073RZ_blinky/Src/stm32l0xx_it.c
@@ -0,0 +1,106 @@
+/** 
+  ******************************************************************************
+  * @file    Examples_LL/GPIO/GPIO_InfiniteLedToggling/Src/stm32l0xx_it.c
+  * @author  MCD Application Team
+  * @brief   Main Interrupt Service Routines.
+  *          This file provides template for all exceptions handler and
+  *          peripherals interrupt service routine.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2016 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l0xx_it.h"
+
+/** @addtogroup STM32L0xx_LL_Examples
+  * @{
+  */
+
+/** @addtogroup GPIO_InfiniteLedToggling
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/******************************************************************************/
+/*            Cortex-M0+ Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles PendSVC exception.
+  * @param  None
+  * @retval None
+  */
+void PendSV_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+}
+
+/******************************************************************************/
+/*                 STM32L0xx Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (GPIO), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32l0xx.s).                                               */
+/******************************************************************************/
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
